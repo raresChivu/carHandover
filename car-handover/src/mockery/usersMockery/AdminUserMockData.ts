47d939c3d3301ba3edb932c6e35d44509d88831f
raresChivu <rares.chivu@vodafone.com>
2025-07-04 15:56:12 +0300

    added mockery
    AdminUsers: list of company email extensions
    NormalUsers: list of generic email addresses

---
+++ car-handover/src/mockery/usersMockery/AdminUserMockData.ts
@@ -0,0 +1,12 @@
+export const adminEmailDomains = [
+  "microsoft.com",
+  "google.com",
+  "apple.com",
+  "amazon.com",
+  "ibm.com",
+  "oracle.com",
+  "adobe.com",
+  "intel.com",
+  "salesforce.com",
+  "tesla.com",
+];
---
+++ car-handover/src/mockery/usersMockery/NormalUserMockData.ts
@@ -0,0 +1,12 @@
+export const normalUserEmails = [
+  "john.doe@gmail.com",
+  "jane.smith@yahoo.com",
+  "alex.brown@outlook.com",
+  "lisa.white@hotmail.com",
+  "michael.green@protonmail.com",
+  "emma.jones@icloud.com",
+  "david.miller@mail.com",
+  "sophia.wilson@zoho.com",
+  "daniel.moore@gmx.com",
+  "olivia.taylor@aol.com",
+];
